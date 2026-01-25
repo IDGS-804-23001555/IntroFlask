08965d114fc9efb93c7fd7bc1c1cad206a52b0b4
Yutzilo <sanchez.yutzil02@gmail.com>
2026-01-24 20:16:36 -0600

    distancia

---
+++ app.py
@@ -0,0 +1,120 @@
+from flask import Flask, render_template, request
+import math
+from flask_wtf.csrf import CSRFProtect
+import forms
+
+
+app = Flask(__name__)
+app.secret_key = 'clave_Secreta'
+csrf = CSRFProtect(app)
+
+@app.route("/")
+def index():
+    title="IDGS804 - intro flask"
+    listado=["juan", "juanito"]
+    return render_template("index.html", title=title, listado=listado)
+    
+
+@app.route("/saludo1")
+def saludo1():
+    return render_template("saludo1.html")
+
+@app.route("/saludo2")
+def saludo2():
+    return render_template("saludo2.html")
+
+
+
+
+@app.route("/hola")
+def func():
+    return "debug activado"
+
+@app.route("/user/<string:user>")
+def user(user):
+    return f"Hola {user}"
+
+@app.route("/numero/<int:n>")
+def numero(n):
+    return f"<h1>Numero: {n}</h1>"
+
+@app.route("/user/<int:id>/<string:username>")
+def username(id, username):
+    return f"<h1>Userid: {id}, username: {username}</h1>"
+
+@app.route("/suma/<float:n1>/<float:n2>")
+def suma(n1, n2):
+    return f"<h1>La suma es: {n1 + n2}</h1>"
+
+@app.route("/default/")
+@app.route("/default/<string:parm>")
+def func2(parm = "juanito perez"):
+    return f"<h1>Hola {parm}</h1>"
+
+@app.route("/operas")
+def operas():
+    return '''
+            <form>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name">
+            <br>
+            <label for="name">apaterno:</label>
+            <input type="text" id="name" name="name">
+            <br>
+            <button type="submit">Submit</button>
+            </form>
+        '''
+        
+        
+@app.route("/operasbas", methods=["GET", "POST"])
+def operasbas():
+    res=None
+    if request.method == 'POST':
+        n1=request.form.get('num1')
+        n2=request.form.get('num2')
+        if request.form.get('operacion')=='suma':
+            res=float(n1)+float(n2)
+        if request.form.get('operacion')=='resta':
+            res=float(n1)-float(n2)
+        if request.form.get('operacion')=='div':
+            res=float(n1)/float(n2)
+        if request.form.get('operacion')=='multi':
+            res=float(n1)*float(n2)
+    return render_template("operasbas.html", res=res)
+    
+
+@app.route("/resultado", methods=["POST", "GET"])
+def result():
+    n1=request.form.get("num1")
+    n2=request.form.get("num2")
+    return f"<h1>La suma es: {float(n1) + float(n2)}</h1>"
+
+@app.route("/distancia", methods=["GET", "POST"])
+def distancia():
+    res = None
+    if request.method == 'POST':
+        x1 = float(request.form.get('x1'))
+        y1 = float(request.form.get('y1'))
+        x2 = float(request.form.get('x2'))
+        y2 = float(request.form.get('y2'))
+        res = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return render_template("distancia.html", res=res)
+    
+@app.route("/alumnos", methods=["GET", "POST"] )
+def alunmos():
+    mat=0
+    nom=""
+    ape=""
+    email=""
+    alumno_class=forms.Userform(request.form)
+    if request.method == 'POST' and alumno_class.validate():
+        mat=alumno_class.matricula.data
+        nom=alumno_class.nombre.data
+        ape=alumno_class.apellido.data
+        email=alumno_class.correo.data
+    return render_template("alumnos.html", form=alumno_class, mat=mat, nom=nom, ape=ape, email=email)
+
+
+if __name__ == '__main__':
+    csrf.init_app(app)
+    app.run(debug=True)
